2e962a7fb3fd2eff545426eeaa448bfe5726ec90
Bungie25 <stephen.heather.84@gmail.com>
2021-03-25 22:15:46 +0000

    Add files via upload

---
+++ userid-webconfig.ps1
@@ -0,0 +1,14 @@
+﻿Copy-Item "web.config" -Destination "C:\temp\web.config"
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\aspnet_regiis.exe -pdf connectionStrings 'C:\Temp'
+$myText = Get-Content C:\Temp\web.config
+$userid = $myText -split ";" | Select-String -Pattern 'UserID'
+$justtheid = $userid -replace '=',' ' 
+$deletefile = Remove-Item "C:\temp\web.config"
+
+
+
+$MsgBox = [System.Windows.Forms.MessageBox]::Show("Thank you, your Userid is: $justtheid")
+If($MsgBox -eq 'OK'){
+    
+    $deletefile
+}
